a15a3769460846aadfb79d2914f7e6708e8d3f74
zgoldstein <zgoldstein@hugeinc.com>
2015-07-30 11:05:01 -0400

    Supports xsrf tokens on webforms. Accepts params in txt format.

--- curl-cannon.sh
+++ curl-cannon.sh
@@ -21,14 +21,14 @@ TOKEN_PARAM="$(echo $TOKEN_LINE | grep -o 'value=[^ >]\+' | sed s/\"//g)"
 JSON_TOKEN="$(echo $TOKEN_LINE | grep -o 'value=[^ >]\+' | sed "s/value=/\"value\":/g")"
 TOKEN=${TOKEN_PARAM#$prefix}
 
-echo "Token: ${TOKEN_LINE}"
+echo "Token: ${TOKEN}"
 
 PARAMS="xsrf="$TOKEN
 while IFS='' read -r line || [[ -n $line ]]; do
   PARAMS="${line}&${PARAMS}"
 done < "params.txt"
-#JSON="$(echo $TOKEN_LINE | grep -o 'value=[^ >]\+' | sed "s/value=/\"value\":/g")"
 
+echo "JSON: ${PARAMS}"
 #cp post.json temp.json
 #sed -i '$s/}/,\n${JSON}/' temp.json
 JSON="$(jq -c ".xsrf=\"${TOKEN}\"" post.json)"
@@ -38,4 +38,4 @@ JSON="$(jq -c ".xsrf=\"${TOKEN}\"" post.json)"
 # -H sets header
 # -d sets as POST. @ preface for filename
 #curl -s -X POST -H "Accept: application/json" -d @temp.json $1
-curl -s -X POST -H "Accept: application/json" -d $PARAMS $1
+curl -s -X POST -H "Accept: application/json" -d "${PARAMS}" $1
